229d161145aa501c476726fd1e0a139524391045
chetan14203 <109654816+chetan14203@users.noreply.github.com>
2023-08-04 13:31:48 +0530

    Updated

--- controller/userController.js
+++ controller/userController.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
 const getUser = async (req,res) => {
-    const user = await User.find().select("-password");
+    const user = await User.find().select("-password -tokens");
     if(!user){
         return res.status(404).json("User is not registered");
     }
@@ -22,7 +22,7 @@ const getUserById = async (req,res) => {
             {worklocation : {$regex : id}},
             {position : {$regex : id}},
         ]
-    }).select("-password");
+    }).select("-password -tokens");
     if(!userById){
         return res.status(404).json("No record is available");
     }
