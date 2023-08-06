3b450e7280067fb0e3f60b8e609cc6a5c360da9c
chetan14203 <109654816+chetan14203@users.noreply.github.com>
2023-08-06 19:11:19 +0530

    Update

--- validations/signupvalidations.js
+++ validations/signupvalidations.js
@@ -1,9 +1,8 @@
 const {check} = require("express-validator");
 
 exports.signUpVal = [
-    check("fullName","Please enter FullName"),
-    check("email").isEmail(),
-    check("mobileNumber","Please Enter 10 digit Mobile number").isLength({
+    check("fullName",{message :"Please enter FullName"}),
+    check("mobileNumber",{message :"Please Enter 10 digit Mobile number"}).isLength({
         min : 10,
         max : 10
     })
